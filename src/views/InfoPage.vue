ade66e4131c89288a4e4286f3c229ce0ed35b081
Ruby Benitez <YOUR_GITHUB_EMAIL@example.com>
2026-02-22 15:02:57 -0600

    info changes

--- src/views/InfoPage.vue
+++ src/views/InfoPage.vue
@@ -21,9 +21,17 @@ const bio = ref({
   <main class="flex min-h-screen flex-col items-center px-4 py-8 sm:py-12">
     <RouterLink
       to="/"
-      class="mt-8 text-gray-400 underline-offset-4 transition-colors duration-200 hover:text-green-400 hover:underline"
+      class="my-8 self-start text-gray-400 underline-offset-4 transition-colors duration-200 hover:text-green-400 hover:underline"
     >
       ← Back To Links
     </RouterLink>
+
+    <!-- Profile Card -->
+    <div
+      class="2xl w-full max-w-lg rounded border-gray-700 bg-gray-800 p-6 shadow-xl shadow-gray-900/50 sm:p-8"
+    >
+      <!-- Header -->
+      <div class="mb-6 flex flex-col items-center text-center sm:flex-row sm:text-left"></div>
+    </div>
   </main>
 </template>
